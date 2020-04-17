05d793d996718828fed2f2b3083e14f386a12eee
Kim <adriamartinez94@gmail.com>
2020-04-17 21:56:48 +0200

    Animacion rocas hechas en zona 2.5D ruinas

---
+++ Assets/CrushingStone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CrushingStone : MonoBehaviour
+{
+    Vector3 _originalPosition;
+    private void Start()
+    {
+        _originalPosition = transform.position;
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOShakePosition(0.3f, .5f));
+        sequence.Append(transform.DOMoveY(transform.position.y - 6.3f, .5f));
+        sequence.AppendInterval(2);
+        sequence.Append(transform.DOMoveY(_originalPosition.y, .5f));
+        
+        sequence.SetLoops(-1);
+    }
+}
