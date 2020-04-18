7f3d7c68cc6d4ce13f8f1353f71cd14116cfd242
Kim <adriamartinez94@gmail.com>
2020-04-18 21:44:17 +0200

    Puzzle 2, columnas se ponen verde cuando estan bien

---
+++ Assets/Puzzle2Manager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle2Manager : MonoBehaviour
+{
+    public bool _column1;
+    public bool _column2;
+    public bool _column3;
+
+    [Header("Animator columnas")]
+
+    [SerializeField]
+    Animator animColumn_1;
+
+    [SerializeField]
+    Animator animColumn_2;
+
+    [SerializeField]
+    Animator animColumn_3;
+
+
+    public void Column1(bool b)
+    {
+        _column1 = b;
+        animColumn_1.SetTrigger("SwitchColor");
+        CheckSoulution();
+    }
+    public void Column2(bool b)
+    {
+        _column2 = b;
+        animColumn_2.SetTrigger("SwitchColor");
+        CheckSoulution();
+    }
+    public void Column3(bool b)
+    {
+        _column3 = b;
+        animColumn_3.SetTrigger("SwitchColor");
+        CheckSoulution();
+    }
+    void CheckSoulution()
+    {
+        if(_column1 && _column2 && _column3) 
+        {
+            print("Has solucionado el puzzle 2");
+        }
+    }
+}
