a06a046d6197894203416ff5218e924f0d96f842
Kim <adriamartinez94@gmail.com>
2020-05-08 14:38:11 +0200

    Hecho el sistema de snaps del puzzle 3

--- Assets/Scripts/Puzzles and Challenge/RockPuzzle3.cs
+++ Assets/Scripts/Puzzles and Challenge/RockPuzzle3.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -7,9 +8,12 @@ public class RockPuzzle3 : MonoBehaviour
 {
     Vector3 _OriginalPosition;
     BoxCollider boxCollider;
+    Rigidbody rb;
+    bool canSnap = true;
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider>();
+        rb = GetComponent<Rigidbody>();
     }
     private void Start()
     {
@@ -22,9 +26,23 @@ public class RockPuzzle3 : MonoBehaviour
     IEnumerator ResetPositionCoroutine()
     {
         boxCollider.enabled = false;
+        canSnap = true;
         transform.position = _OriginalPosition;
         yield return new WaitForSeconds(.5f);
         boxCollider.enabled = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Snap") && canSnap)
+        {
+            transform.SetParent(other.transform);
+            transform.DOLocalMove(Vector3.zero, 1).OnComplete(() => FreezePosition());
+        }
+    }
 
+    void FreezePosition()
+    {
+        canSnap = false;
+        rb.isKinematic = true;
     }
 }
