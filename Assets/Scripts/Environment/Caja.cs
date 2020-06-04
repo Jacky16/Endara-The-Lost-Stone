ddea7d7dd1182f7493c0880d3784df2bb190ace4
Kim <adriamartinez94@gmail.com>
2020-06-04 14:37:30 +0200

    Arreglado problema a la hora de destruir las cajas, y ajustes retocados a la hora de destruirla

--- Assets/Scripts/Environment/Caja.cs
+++ Assets/Scripts/Environment/Caja.cs
@@ -34,18 +34,16 @@ public class Caja : MonoBehaviour
     }
     void Explosion()
     {
-        GameObject go = Instantiate(prefabBoxDestroyed, transform.position, transform.rotation) as GameObject;
+        GameObject go = Instantiate(prefabBoxDestroyed, transform.position, Quaternion.identity) as GameObject;
         Rigidbody[] rb = go.GetComponentsInChildren<Rigidbody>();
         _audioSource.PlayOneShot(AudioClipRandom());
         _audioSource.DOFade(0, AudioClipRandom().length);
-        Destroy(go, 2);
+        Destroy(go, 4);
         foreach (Rigidbody r in rb)
         {
             r.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
         }
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<BoxCollider>().enabled = false;
-        Destroy(this.gameObject,1.5f);
+        Destroy(gameObject);
     }
     AudioClip AudioClipRandom()
     {
