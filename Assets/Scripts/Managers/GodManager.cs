4bed533cb1dac83f5a41cd58919e6d66545e8d78
Kim <adriamartinez94@gmail.com>
2020-06-19 20:43:27 +0200

    Teleport hecho en el juego para la presentacion gold

--- Assets/Scripts/Managers/GodManager.cs
+++ Assets/Scripts/Managers/GodManager.cs
@@ -1,66 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 using TMPro;
 
 public class GodManager : MonoBehaviour
 {
     [SerializeField] GameObject canvasModeGod;
     public float unitsGravityModeGod;
-    public TextMeshProUGUI textSpaceUp;
-    public TextMeshProUGUI textSpaceDown;
-    public TMP_InputField inputFieldUnits;
+    [SerializeField]
+    Transform playerTransform;
 
     private void Start()
     {
         unitsGravityModeGod = 5;
+        canvasModeGod.SetActive(false);
 
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F10))
+        if (Input.GetKeyDown(KeyCode.F2))
         {
-           PlayerMovement.isModeGod = !PlayerMovement.IsModeGod();
+            PlayerMovement.isModeGod = !PlayerMovement.IsModeGod();
         }
         if (PlayerMovement.IsModeGod())
         {
             canvasModeGod.SetActive(true);
-            textSpaceUp.text = "Space = Up " + UnitsToJumpInModeGod().ToString() + " Units";
-            textSpaceDown.text = "Space = Down " + UnitsToJumpInModeGod().ToString() + " Units";
+
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
-           
+            playerTransform.name = "Mode God";
+
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.F10))
+            if (Input.GetKeyDown(KeyCode.F2))
             {
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
             }
             canvasModeGod.SetActive(false);
-            //Cursor.visible = false;
-            //Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            playerTransform.name = "Player";
         }
-        
+
+    }
+    public void Teleport(Transform tpPosition)
+    {
+        playerTransform.DOMove(tpPosition.position, .1f);
     }
     public float UnitsToJumpInModeGod()
     {
         return unitsGravityModeGod;
     }
-    public void AssingInputField()
-    {
-        unitsGravityModeGod = float.Parse(inputFieldUnits.text);
-        inputFieldUnits.text = null;
-    }
-    public void Sum2Units()
-    {
-        unitsGravityModeGod += 2;
-    }
-    public void Rest2Units()
-    {
-        unitsGravityModeGod -= 2;
-    }
+   
+  
     
 
 
