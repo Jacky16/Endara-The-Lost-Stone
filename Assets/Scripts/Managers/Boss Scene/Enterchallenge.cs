390266ad373c5a4dcdda2bb1cf91593d9e6f5a1b
Kim <adriamartinez94@gmail.com>
2020-06-21 23:33:37 +0200

    ahora es cuando todo falla o que??, JODER, 3r puta build hecha para la gold

--- Assets/Scripts/Managers/Boss Scene/Enterchallenge.cs
+++ Assets/Scripts/Managers/Boss Scene/Enterchallenge.cs
@@ -40,6 +40,7 @@ public class Enterchallenge : MonoBehaviour
     {
         doorSound = GetComponent<AudioSource>();
     }
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && canEnter || (other.CompareTag("Player") && PlayerMovement.IsModeGod()))
